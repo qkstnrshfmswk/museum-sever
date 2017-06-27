5444e4a4e2d784762c5bf1f35bd58e2daafa7429
qkstnrshfmswk <cjcj558@naver.com>
2017-06-27 19:18:13 +0900

    text

--- routes/about.js
+++ routes/about.js
@@ -7,12 +7,9 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.get('/img', function(req, res, next){
-  res.json([{"info_img": 'http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg'}]);
+router.get('/', function(req, res, next){
+  res.json([{"info_img": 'http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg', "info": 'About museum \n This is museum'}]);
 })
 
-router.get('/info', function(req, res, next){
-  res.json([{"info": 'About museum \n This is museum'}]);
-})
 
 module.exports = router;
