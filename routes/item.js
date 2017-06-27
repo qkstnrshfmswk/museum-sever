7efe96fa6b2e79959eb428da2cdac49aacb9c303
qkstnrshfmswk <cjcj558@naver.com>
2017-06-27 20:46:38 +0900

    item

--- routes/item.js
+++ routes/item.js
@@ -4,21 +4,22 @@ var router = express.Router();
 
 router.get('/:_itemID', function(req, res, next){
   id = req.params._itemID;
+  console.log(id);
   switch(id)
   {
-    case 1:  res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
+    case '1':  res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
                         "item_name" :"item1",
                         "desc": "Item1 description"}]);
     break; 
-    case 2: res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
+    case '2': res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
                         "item_name" :"item1",
                         "desc": "Item1 description"}]);
     break;
-    case 3: res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
+    case '3': res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
                         "item_name" :"item1",
                         "desc": "Item1 description"}]);
     break;
-    case 4: res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
+    case '4': res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
                         "item_name" :"item1",
                         "desc": "Item1 description"}]);
     break;
