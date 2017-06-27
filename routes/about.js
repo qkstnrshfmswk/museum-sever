721645f2fb2b02de7197fd557e307d37a23f9915
qkstnrshfmswk <cjcj558@naver.com>
2017-06-27 19:12:59 +0900

    text

--- routes/about.js
+++ routes/about.js
@@ -11,4 +11,8 @@ router.get('/img', function(req, res, next){
   res.json([{"info_img": 'http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg'}]);
 })
 
+router.get('/info', function(req, res, next){
+  res.json([{"info": 'About museum \n This is museum'}]);
+})
+
 module.exports = router;
