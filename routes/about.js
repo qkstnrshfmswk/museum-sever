388754b04a87a7e0a33711e24a0f68fd667ddc76
qkstnrshfmswk <cjcj558@naver.com>
2017-06-27 20:20:57 +0900

    new line

--- routes/about.js
+++ routes/about.js
@@ -2,13 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
 
-});
 
 router.get('/', function(req, res, next){
-  res.json([{"info": "About museum  This is museum"}]);
+  res.json([{"info_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", "info": "About museum \\n This is museum"}]);
 })
 
 
