71cefee24fd5445a8ccfb0c76be914cc2d98335c
qkstnrshfmswk <cjcj558@naver.com>
2017-06-29 01:14:27 +0900

    exhibit id

--- routes/exhibit.js
+++ routes/exhibit.js
@@ -16,40 +16,52 @@ There life style and culture are exposed impressively below this first basement 
 influencing the subconscious of the visitors that they are at the height to understand and know \
 the information dis-played just below them.<br><br>\
 For the close observation, when the visitors stare down the basement, they are ushered by the museum \
-guides to the adjoining video hall to orient them about the people, culture and the mountains of Khumbu region."}]);
+guides to the adjoining video hall to orient them about the people, culture and the mountains of Khumbu region.",
+                                "section_list":[{"section_name":"Mountain People of Nepal", "section_num": 1}, 
+                                                {"section_name":"Mountain People of the World", "section_num": 2},
+                                                {"section_name": "The Mandala", "section_num" : 3}]}]);
         break;
 
         case '2' : res.json([{"exhibit_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/exhibit_WM.PNG",
                               "exhibit_name": [{"line":"Hall of"}, {"line":"World Mountain"}],
-                              "exhibit_loc": "BF",
-"exhibit_desc" : "world mountain description It shows indigenous habitants of the mountain re-gions of Nepal and the world. \
+                              "exhibit_loc": "GF",
+                              "exhibit_desc" : "It shows indigenous habitants of the mountain re-gions of Nepal and the world. \
 There life style and culture are exposed impressively below this first basement ball, \
 influencing the subconscious of the visitors that they are at the height to understand and know \
 the information dis-played just below them.<br><br>\
 For the close observation, when the visitors stare down the basement, they are ushered by the museum \
-guides to the adjoining video hall to orient them about the people, culture and the mountains of Khumbu region."}]);
+guides to the adjoining video hall to orient them about the people, culture and the mountains of Khumbu region.",
+                                "section_list":[{"section_name":"Mountain People of Nepal", "section_num": 1}, 
+                                                {"section_name":"Mountain People of the World", "section_num": 2},
+                                                {"section_name": "The Mandala", "section_num" : 3}]}]);
         break;
 
-        case '3' : res.json([{"exhibit_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/exhibit_MA.PNG",
+        case '3' :  res.json([{"exhibit_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/exhibit_MA.PNG",
                               "exhibit_name": [{"line":"Hall of"}, {"line":"Mountain Activities"}],
-                              "exhibit_loc": "BF",
-                              "exhibit_desc" : "MA It shows indigenous habitants of the mountain re-gions of Nepal and the world. \
+                              "exhibit_loc": "GF",
+                              "exhibit_desc" : "It shows indigenous habitants of the mountain re-gions of Nepal and the world. \
 There life style and culture are exposed impressively below this first basement ball, \
 influencing the subconscious of the visitors that they are at the height to understand and know \
 the information dis-played just below them.<br><br>\
 For the close observation, when the visitors stare down the basement, they are ushered by the museum \
-guides to the adjoining video hall to orient them about the people, culture and the mountains of Khumbu region."}]);
+guides to the adjoining video hall to orient them about the people, culture and the mountains of Khumbu region.",
+                                "section_list":[{"section_name":"Mountain People of Nepal", "section_num": 1}, 
+                                                {"section_name":"Mountain People of the World", "section_num": 2},
+                                                {"section_name": "The Mandala", "section_num" : 3}]}]);
         break;
 
-        case '4' : res.json([{"exhibit_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/exhibit_AE.PNG",
+        case '4' :  res.json([{"exhibit_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/exhibit_AE.PNG",
                               "exhibit_name": [{"line":"Hall of"}, {"line":"Associates\' Exhibition"}],
-                              "exhibit_loc":"BF",
-                              "exhibit_desc" : "AE It shows indigenous habitants of the mountain re-gions of Nepal and the world. \
+                              "exhibit_loc": "GF",
+                              "exhibit_desc" : "It shows indigenous habitants of the mountain re-gions of Nepal and the world. \
 There life style and culture are exposed impressively below this first basement ball, \
 influencing the subconscious of the visitors that they are at the height to understand and know \
 the information dis-played just below them.<br><br>\
 For the close observation, when the visitors stare down the basement, they are ushered by the museum \
-guides to the adjoining video hall to orient them about the people, culture and the mountains of Khumbu region."}]);
+guides to the adjoining video hall to orient them about the people, culture and the mountains of Khumbu region.",
+                                "section_list":[{"section_name":"Mountain People of Nepal", "section_num": 1}, 
+                                                {"section_name":"Mountain People of the World", "section_num": 2},
+                                                {"section_name": "The Mandala", "section_num" : 3}]}]);
         break;
     }
 });
