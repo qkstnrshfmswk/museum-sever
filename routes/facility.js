2b7aac4a842f41fd2d7597fcc79b8ed460e4db6d
qkstnrshfmswk <cjcj558@naver.com>
2017-06-28 09:49:13 +0900

    error

--- routes/facility.js
+++ routes/facility.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.json({"facilities":"Library"},
+    res.json([{"facilities":"Library"},
              {"facilities":"Gorkha Village Restaurant"},
              {"facilities":"Souvenir Shop"},
              {"facilities": "Garden"},
-             {"facilities":"Wall climbing"})
+             {"facilities":"Wall climbing"}])
 });
 
 router.get('/libraries', function(req, res, next){
