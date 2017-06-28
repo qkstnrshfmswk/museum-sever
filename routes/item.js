7f7f6436e67ec3b32f597544e6efec8b53372049
qkstnrshfmswk <cjcj558@naver.com>
2017-06-28 09:10:25 +0900

    v1

--- routes/item.js
+++ routes/item.js
@@ -11,15 +11,15 @@ router.get('/:_itemID', function(req, res, next){
                         "desc": "Item1 description"}]);
     break; 
     case '2': res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
-                        "item_name" :"item1",
+                        "item_name" :"item2",
                         "desc": "Item1 description"}]);
     break;
     case '3': res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
-                        "item_name" :"item1",
+                        "item_name" :"item3",
                         "desc": "Item1 description"}]);
     break;
     case '4': res.json([{"item_img": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg", 
-                        "item_name" :"item1",
+                        "item_name" :"item4",
                         "desc": "Item1 description"}]);
     break;
   }
