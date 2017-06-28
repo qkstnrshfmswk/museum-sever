5192fbf3dded48dd0f3f3de045e154654de1a72a
qkstnrshfmswk <cjcj558@naver.com>
2017-06-28 14:31:42 +0900

    main

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-  res.json([{"img_path": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/1.jpg"},
+  res.json([{"img_path": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/main.jpg"},
             {"img_path": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/2.jpg"},
             {"img_path": "http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/public/3.jpg"}]);
 })
