a656ee1b5da3b2ff5cbe0fd015050138e7c629dc
qkstnrshfmswk <cjcj558@naver.com>
2017-08-09 00:52:32 +0900

    edit maps

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use('/museum_info', museum_info);
 app.use('/exhibit', exhibit);
 app.use('/exhibitList', exhibitList);
 app.use('/map', map);
+app.use('/maps', maps);
 app.use('/public', express.static(path.join(__dirname, '/public/images')));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
